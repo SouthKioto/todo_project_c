bb3bdce331120f2e0959020e864585bc410b3d2d
Bartlomiej <bolisegabartek6@gmail.com>
2025-11-20 21:15:43 +0100

    create ids function

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -22,28 +23,39 @@ Funkcje:
   Plik z danymi: ~/.todo_db.txt (każdy wiersz: id,title,done)
 */
 
-void get_last_id(const char *file_name) {
+char *get_todo_ids(const char *file_name) {
   FILE *file = fopen(file_name, "r");
 
   if (!file) {
-    printf("Nie moge otworzyc pliku");
+    printf("Nie mogę otworzyć pliku\n");
+    return 0;
   }
 
-  char todo_ids[100];
-
   char line[256];
-
-  printf("Dane\n");
+  static char ids[256];
+  int count = 0;
 
   while (fgets(line, sizeof(line), file)) {
-    // TODO: jak zdobyc id?
-    if (strcpy(line, "Todo_id:")) {
+    if (strncmp(line, "Todo_id", 7) == 0) {
+      size_t len = strlen(line);
 
-      printf("%s", line);
+      if (len > 0 && line[len - 1] == '\n') {
+        line[len - 1] = '\0';
+        len--;
+      }
+
+      if (len > 0) {
+        ids[count] = line[len - 1];
+        count++;
+      }
     }
   }
 
   fclose(file);
+
+  ids[count] = '\0';
+
+  return ids;
 }
 
 void todo_add(const char *task_title, const char *file_name) {
@@ -56,26 +68,34 @@ void todo_add(const char *task_title, const char *file_name) {
   }
 
   // additional data
-  int todo_id = 1;
+  // check last_id
+  char *ids;
+  char last_id;
+  ids = get_todo_ids(file_name);
+  if (ids != NULL) {
+    size_t len = strlen(ids);
+    last_id = ids[len - 1];
+  } else {
+    last_id = 1;
+  }
 
+  // add time
   time_t now;
   time(&now);
   char *date_time = ctime(&now);
 
-  // FIXME: add and check id in file
-
-  char *message = "Todo_id: %d\nTask Name: %s\nTask Time: %s\nDone: false\n";
+  char *message = "Todo_id: %d\nTask Name: %s\nTask Time: %sDone: false\n";
 
   //  check file exist
   if (file_exist == NULL) {
     // create and write
     file = fopen(file_name, "w");
-    fprintf(file, message, todo_id, task_title, date_time);
+    fprintf(file, message, last_id, task_title, date_time);
     printf("Plik zostal utworzony pod nazwa %s \n", file_name);
   } else {
     // update file
     file = fopen(file_name, "a");
-    fprintf(file, message, todo_id, task_title, date_time);
+    fprintf(file, message, last_id, task_title, date_time);
     printf("Zadanie '%s' zostalo dodane do pliku %s \n", task_title, file_name);
   }
 
@@ -108,9 +128,7 @@ int main() {
   const char *file_name = "todo_db.txt";
 
   // todo_add("Zadanie1", file_name);
-  //  todo_read();
-
-  get_last_id(file_name);
+  // todo_read(file_name);
 
   return 0;
 }
